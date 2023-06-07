c4a118a14bb8d88f8116e8d6a709bbc009403e77
Kiran-Alex <98690069+Kiran-Alex@users.noreply.github.com>
2023-06-07 11:26:48 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const stringSimilarity = require('string-similarity');
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on('ready', () => {
+  console.log(`Bot logged in as ${client.user.tag}`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  console.log(message.content)
+})
+
+client.on('messageCreate', (message) => {
+  const triggerWords = ['kirat bhaiya', 'kirat sir', 'kirat baia', 'kirat bhaaiyaa']
+  const content = message.content.toLowerCase();
+  const matches = stringSimilarity.findBestMatch(content, triggerWords);
+  const bestMatch = matches.bestMatch;
+
+  if (bestMatch.rating >= 0.75) {
+    const user = message.author;
+    user.send(`Hey ${message.author} , Stop calling Harkirat bhaiya or sir just Harkirat is fine!`);
+  }
+});
+
+client.login("PLACE YOUR TOKEN HERE");
